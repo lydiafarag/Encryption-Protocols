b0561a583e6edbade783e3b90fc39572bde31865
Lydia Farag <77176132+lydiafarag@users.noreply.github.com>
2022-08-02 20:58:53 -0400

    Error while uploading file

---
+++ Cerebus.java
@@ -0,0 +1,41 @@
+mport java.util.ArrayList;
+
+public class Cerebus extends Protocol{
+    private final int CEREBUS_INIT_VALUE= 32;
+    private final int CEREBUS_ALPH_SIZE= 128;
+
+    public Cerebus() {
+        ArrayList<Integer> key_1 = new ArrayList<>();
+        ArrayList<Integer> key_2 = new ArrayList<>();
+
+        key_1.add(18);
+        key_2.add(31);
+
+        keys.add(key_1);
+        keys.add(key_2);
+    }
+
+        @Override
+        public String encryption(int type, String notification) {
+            ArrayList<Integer> key = keys.get(type);
+            StringBuilder out = new StringBuilder();
+            String use = notification.toLowerCase();
+            for(char a : use.toCharArray()) {
+                out.append((char)(((a + key.get(0) - CEREBUS_INIT_VALUE) % CEREBUS_ALPH_SIZE) + CEREBUS_INIT_VALUE)+"");
+            }
+            return out.toString();
+        }
+
+        @Override
+        public String decryption(int type, String notification) {
+            ArrayList<Integer> key = keys.get(type);
+            StringBuilder out = new StringBuilder();
+            String use = notification.toLowerCase();
+            for(char a : use.toCharArray()) {
+                out.append((char)(((a - key.get(0)  - CEREBUS_INIT_VALUE + CEREBUS_ALPH_SIZE) % CEREBUS_ALPH_SIZE) + CEREBUS_INIT_VALUE)+"");
+            }
+            return out.toString();
+        }
+
+}
+
